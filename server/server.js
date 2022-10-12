94aec6590f7ff28ad092b6fe6af60c344719de7f
VenireVidereVincere <alan_jurado_canseco@hotmail.com>
2022-10-12 12:43:08 -0500

    Added start script and  fixed port used by Heroku

--- server/server.js
+++ server/server.js
@@ -2,6 +2,7 @@ var express = require('express')
 var app = express()
 let path = require('path')
 const publicPath = path.join(__dirname,"..", "public")
+const port = process.env.PORT || 3000;
 
 app.use(express.static(publicPath))
 
@@ -9,6 +10,6 @@ app.get("*", (req, res) => {
     res.sendFile(path.join(publicPath,"index.html"))
 })
 
-app.listen(3000, () => {
+app.listen(port, () => {
     console.log("server is up on port 3000.")
 })
